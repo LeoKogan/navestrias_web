50f37556f1d837bd4292c5277e7579f783e0dfd6
Nardo <nardo.kogan@gmail.com>
2025-12-25 14:22:11 -0700

    first version

---
+++ form.php
@@ -0,0 +1,12 @@
+﻿<?
+   $to = "info@navestrias.com";
+   $subject = "Consulta enviada desde Pagina Web";   
+   $msg  = "$nom, le envia el siguiente mensaje:\n\n";
+   $msg .= "Teléfono: $telefono\n";
+   $msg .= "E-mail: $mailfrom\n";
+   $msg .= "Comments: $body\n";
+   mail($to, $subject, $msg, "From: $mailfrom");
+
+?>
+
+
